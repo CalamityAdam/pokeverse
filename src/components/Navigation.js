09d8a92cd7f525db08fff0f265f8ac932f10b5ca
Adam Sisk <adamsisk91@gmail.com>
2022-06-12 19:07:22 -0500

    formatting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import squirtle from '../../public/squirtle.png';
-import { NavLink } from 'react-router-dom';
 
 function Navigation() {
   return (
